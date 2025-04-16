59687dee0783d6fc63a12fc7e3d0155736e54888
wyllamcoelhoads <wyllamcoelho8@gmail.com>
2025-04-15 22:36:17 -0300

    atualizando

--- Program.cs
+++ Program.cs
@@ -1,22 +1,25 @@
-﻿Random dice = new Random();
+﻿/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
 
-int total = roll1 + roll2 + roll3;
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
+int letterCount = 0;
+
+foreach (char letter in message)
 {
-    if ((roll1 == roll2) && (roll2 == roll3)) 
+    if (letter == 'o')
     {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6; 
-    } 
-    else 
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
+        letterCount++;
     }
 }
+
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
