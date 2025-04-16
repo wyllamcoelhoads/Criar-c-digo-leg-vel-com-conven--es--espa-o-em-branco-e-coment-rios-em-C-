ff6e1f5d3229f2f7e3bcfa7c35b22be478c988d6
wyllamcoelhoads <wyllamcoelho8@gmail.com>
2025-04-15 22:28:55 -0300

    Inciando

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string firstName = "Bob";
+int widgetsSold = 7;
+//Testeing a charge to the message.
+//int widgetsSold = 7;
+//Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+Console.WriteLine($"{firstName} purchased {widgetsSold} widgets.");
